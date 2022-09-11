625d57628cdc466daf2716dd7156b50248d53981
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-09-11 09:37:35 -0500

    Coming soon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import isotype from "../public/isotype-white.png";
+
 export default function Home() {
   return (
-    <div className='bg-black text-white flex justify-center items-center h-screen'>
-      <h1>Underconstruction</h1>
+    <div className='bg-gradient-to-l to-[#c621e5] from-[#720cd4] text-white flex justify-center items-center h-screen'>
+      <div className='flex flex-col items-center'>
+        <div className='animate-[hearting_1s_ease-in-out_infinite]'>
+          <Image src={isotype} width={200} height={200} alt='Jorge Olarte' />
+        </div>
+        <h1 className='text-xl'>Coming soon</h1>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        hearting: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.05)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
